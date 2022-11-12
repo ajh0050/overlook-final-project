0c230e158292636ceb249a87925f780294c2f946
Adam Hughes <adamjordanhughes@gmail.com>
2022-11-12 12:25:46 -0500

    Fetch the api data

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,4 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
+
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
@@ -7,5 +6,29 @@ import './css/styles.css';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+let customers
+let rooms
+let bookings
 
-console.log('This is the JavaScript entry file - your code begins here.');
+function apiCalls () {
+    let customersFetch = fetch('http://localhost:3001/api/v1/customers').then((response) => response.json()).then((data) => customers=data)
+    let roomsFetch = fetch('http://localhost:3001/api/v1/rooms').then((response) => response.json()).then((data) => rooms=data)
+    let bookingsFetch = fetch('http://localhost:3001/api/v1/bookings').then((response) => response.json()).then((data) => bookings=data)
+    console.log(bookings)
+
+    return Promise.all([customersFetch,roomsFetch,bookingsFetch]).then((data)=> {
+        customers = data[0]
+        rooms = data[1]
+        bookings = data[2]
+        loadHandler()
+    })
+}
+
+function loadHandler() {
+    console.log("customers",customers)
+    console.log("rooms",rooms)
+    console.log("bookings",bookings)
+}
+
+
+window.addEventListener("load", apiCalls())
